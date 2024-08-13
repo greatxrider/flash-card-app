b3c05cb204ab9ae9a8367c753ac7386b663ed652
greatxrider <daligdig.jephmari@gmail.com>
2024-08-13 10:20:57 +0800

    update appjs

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+// Use Helmet to set security-related HTTP headers
+app.use(helmet());
+
+// Configure CSP to allow font sources
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com", "https://flash-card-891ku8hgk-greatxriders-projects.vercel.app"],
+        styleSrc: ["'self'", "https://fonts.googleapis.com"],
+        scriptSrc: ["'self'"],
+        imgSrc: ["'self'"],
+        connectSrc: ["'self'"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+    }
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/static', express.static('public'));
@@ -28,7 +45,7 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 
-const port = process.env.PORT || 3000;
+const port = 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
